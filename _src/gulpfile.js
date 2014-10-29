75df5a858b7cc0754971b4c762f354d71f8c6de9
Michael Groshans <mgroshans@users.noreply.github.com>
2014-10-28 21:21:23 -0700

    Include first example post and ditch handlebars for swig
    Lots of changes since it's still early on. Basically handlebars was
    nice, but didn't really have the inheritence I wanted so I ditched it
    for swig.
    
    Example posts are now generated and I think I'll try a structure as
    illustrated by example-post where it's id#slug and then there's a
    template and some metadata for the post in the directory. Really id and
    slug are also metadata, but keeping id at the directory level makes it
    hopefully less likely that I screw up and duplicate ids (since I'll
    probably be doing posts by hand for a while). Keeping the slug at the
    directory level just helps me find posts.
    
    Also added a gulp clean. To prevent the static assets from disappearing
    forever when cleaned, I also added a static folder and assets will get
    copied out of it.

--- _src/gulpfile.js
+++ _src/gulpfile.js
@@ -1,29 +1,66 @@
+var Q = require('q');
+var del = require ('del');
 var gulp = require('gulp');
 var path = require('path');
+var extend = require('extend');
+var data = require('gulp-data');
+var swig = require('gulp-swig');
 var through = require('through2');
 var rename = require('gulp-rename');
-var merge = require('merge-stream');
-var Handlebars = require('handlebars');
 
-gulp.task('partials', function () {
-    return gulp.src('./partials/**/*.hbs')
+var posts = [];
+var getPost = function () {
+    var regex = /^(\d+)#(.*)/;
+    return function (file) {
+        var result = path.dirname(file.relative).match(regex);
+        return {
+            id: +result[1],
+            slug: result[2]
+        }
+    };
+}();
+
+gulp.task('index', ['post-metadata'], function () {
+    return gulp.src('./templates/index.html')
+        .pipe(data({ posts: posts }))
+        .pipe(swig())
+        .pipe(gulp.dest('../'));
+});
+
+gulp.task('post-metadata', function () {
+    return gulp.src('./posts/*/metadata.json')
         .pipe(through.obj(function (file, encoding, callback) {
-            Handlebars.registerPartial(path.basename(file.path, '.hbs'), file.contents.toString());
+            var post = extend(getPost(file), JSON.parse(file.contents));
+            posts[post.id] = post;
+            this.push(file);
             return callback();
         }));
 });
 
-gulp.task('templates', ['partials'], function () {
-    return gulp.src('./templates/**/*.hbs')
+gulp.task('posts', ['post-metadata'], function () {
+    return gulp.src('./posts/*/template.html')
+        .pipe(data(function (file) {
+            return posts[getPost(file).id];
+        }))
+        .pipe(swig())
         .pipe(through.obj(function (file, encoding, callback) {
-            file.contents = new Buffer(Handlebars.compile(file.contents.toString())());
+            // can't use gulp-rename because that only exposes the path
+            file.path = path.join(file.base, file.data.slug + '.html');
             this.push(file);
             return callback();
         }))
-        .pipe(rename({
-            extname: '.html'
-        }))
-        .pipe(gulp.dest('../'));
+        .pipe(gulp.dest('../posts'));
 });
 
-gulp.task('default', ['templates']);
+gulp.task('static', function () {
+    return gulp.src('./static/**/*')
+        .pipe(gulp.dest('../'))
+});
+
+gulp.task('default', ['static', 'index', 'posts']);
+
+gulp.task('clean', function (callback) {
+    del('../!(_*)', {
+            force: true
+        }, callback);
+});
