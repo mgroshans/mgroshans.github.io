689404790fd9c4eb0188a41b4831ca022fc5dcfc
Michael Groshans <mgroshans@users.noreply.github.com>
2015-01-02 23:27:22 -0800

    Add some initial styles
    Pulled in gulp-less and gulp-concat to generate the actual CSS and
    gulp-plumber because less was failing silently and plumber at least logs
    to the console. I feel like I could probably do some more investigation
    into gulp error handling since this feels like a bad default, but easier
    to just throw plumber() at everything for now.
    
    Grabbed Source Sans Pro from google webfonts courtesy of
    https://google-webfonts-helper.herokuapp.com/fonts and included the 400
    and 700 weight woff/woff2 versions. This means we'll fallback to
    "sans-serif" in IE8, but I don't really want to use the ugly "?#iefix"
    since this is just a personal blog no one is going to read.
    
    Added some LESS to actually make use of the font and begin some initial
    styling. Still pretty rough for now, but I wanted to checkpoint here
    before I start experimenting with more complex styles.
    
    Finally I cleaned up the base.html a little bit to add <!DOCTYPE html>
    and a few meta tags that seemed useful.

--- _src/gulpfile.js
+++ _src/gulpfile.js
@@ -4,9 +4,12 @@ var gulp = require('gulp');
 var path = require('path');
 var extend = require('extend');
 var data = require('gulp-data');
+var less = require('gulp-less');
 var swig = require('gulp-swig');
 var through = require('through2');
+var concat = require('gulp-concat');
 var rename = require('gulp-rename');
+var plumber = require('gulp-plumber');
 var markedSwig = require('swig-marked');
 
 var posts = [];
@@ -23,6 +26,7 @@ var getPost = function () {
 
 gulp.task('index', ['post-metadata'], function () {
     return gulp.src('./templates/index.html')
+        .pipe(plumber())
         .pipe(data({ posts: posts }))
         .pipe(swig({
             setup: function (swig) {
@@ -35,6 +39,7 @@ gulp.task('index', ['post-metadata'], function () {
 
 gulp.task('post-metadata', function () {
     return gulp.src('./posts/*/metadata.json')
+        .pipe(plumber())
         .pipe(through.obj(function (file, encoding, callback) {
             var post = extend(getPost(file), JSON.parse(file.contents));
             posts[post.id] = post;
@@ -45,6 +50,7 @@ gulp.task('post-metadata', function () {
 
 gulp.task('posts', ['post-metadata'], function () {
     return gulp.src('./posts/*/template.html')
+        .pipe(plumber())
         .pipe(data(function (file) {
             return posts[getPost(file).id];
         }))
@@ -60,10 +66,19 @@ gulp.task('posts', ['post-metadata'], function () {
 
 gulp.task('static', function () {
     return gulp.src('./static/**/*')
+        .pipe(plumber())
         .pipe(gulp.dest('../'))
 });
 
-gulp.task('default', ['static', 'index', 'posts']);
+gulp.task('styles', function() {
+    return gulp.src('./less/*.less')
+        .pipe(plumber())
+        .pipe(less())
+        .pipe(concat('fail.css'))
+        .pipe(gulp.dest('../css/'));
+});
+
+gulp.task('default', ['static', 'styles', 'index', 'posts']);
 
 gulp.task('clean', function (callback) {
     del('../!(_*)', {
